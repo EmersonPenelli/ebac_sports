dcd471e54fdd9a26974aba9af9ba16a2034b6c1d
EmersonPenelli <emersonpenelli@gmail.com>
2024-08-08 19:05:03 -0300

    implementado redux no projeto para gerenciamento de estado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,13 @@
+import { useSelector } from 'react-redux'
+import { paraReal } from '../Produto'
+import { RootReducer } from '../../store'
+
 import * as S from './styles'
 
-import { Produto } from '../../App'
-
 import cesta from '../../assets/cesta.png'
-import { paraReal } from '../Produto'
-
-type Props = {
-  itensNoCarrinho: Produto[]
-  favoritos: Produto[]
-}
 
 const Header = ({ itensNoCarrinho, favoritos }: Props) => {
-  const valorTotal = itensNoCarrinho.reduce((acc, item) => {
+  const valorTotal = itensProduto.reduce((acc, item) => {
     acc += item.preco
     return acc
   }, 0)
@@ -20,10 +16,10 @@ const Header = ({ itensNoCarrinho, favoritos }: Props) => {
     <S.Header>
       <h1>EBAC Sports</h1>
       <div>
-        <span>{favoritos.length} favoritos</span>
-        <img src={cesta} />
+        <span>{itensFavoritos.length} favoritos</span>
+        <img src={cesta} title="cesta do carrinho" />
         <span>
-          {itensNoCarrinho.length} itens, valor total: {paraReal(valorTotal)}
+          {itensProduto.length} itens, valor total: {paraReal(valorTotal)}
         </span>
       </div>
     </S.Header>
